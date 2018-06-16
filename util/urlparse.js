a529d0ed33d501c22dc5c697a2b0243dea3b436a
James Stine <leon.blade@gmail.com>
2018-06-16 13:10:58 -0400

    Add additional functionality to urlparse util

--- util/urlparse.js
+++ util/urlparse.js
@@ -1,17 +1,18 @@
-
+/**
+ * Formats the URL to remove invalid parts
+ * @param {String} url 
+ */
 module.exports = url => {
-
     if (!url)
         return '';
-
     if (url.indexOf('@') != -1)
         return '';
-
     if (url.indexOf(',') != -1)
         url = url.split(',')[0];
-
     if (!url.startsWith('http'))
         url = 'http://' + url;
+    if (url.endsWith(';'))
+        url = url.slice(0, -1);
 
-    return url;
+    return url.trim();
 };
