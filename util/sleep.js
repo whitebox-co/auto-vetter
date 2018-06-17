179fa3fcd308e415f28fe97578b8c3aad2de3e26
James Stine <leon.blade@gmail.com>
2018-06-17 00:33:32 -0400

    Add jsdoc to sleep

--- util/sleep.js
+++ util/sleep.js
@@ -1,6 +1,8 @@
 /**
  * sleep.js
  * A simple async sleep
+ * @param {Number} timeout Time in milliseconds
+ * @returns {Promise} Promise that completes when time is done
  */
 module.exports = timeout => {
     return new Promise((resolve, reject) => {
